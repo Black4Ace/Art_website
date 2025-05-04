95de03cb869d99f98c3fc091c3462fcefb2fd814
RachelKlompasArt <207891391+RachelKlompasArt@users.noreply.github.com>
2025-05-04 16:55:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// Google Analytics tracking setup
+(function() {
+  const gaScript = document.createElement('script');
+  gaScript.async = true;
+  gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-MVXJ44FZJV';
+  document.head.appendChild(gaScript);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag() { dataLayer.push(arguments); }
+  window.gtag = gtag;
+
+  gtag('js', new Date());
+  gtag('config', 'G-MVXJ44FZJV');
+})();
 
 const imageGroups = {
     group1: [
